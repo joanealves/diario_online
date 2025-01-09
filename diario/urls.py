543532a52bc5d3043e8ae5f1f6b44c9c3a12bb98
Joane <joanealves2011@gmail.com>
2025-01-09 14:08:15 -0300

    Update home and views

--- diario/urls.py
+++ diario/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='diario'),
     path('escrever/', views.escrever, name='escrever'),
-    # path('cadastrar_pessoa/', views.cadastrar_pessoa, name='cadastrar_pessoa'),
+    path('cadastrar_pessoa/', views.cadastrar_pessoa, name='cadastrar_pessoa'),
 ]
--- diario/views.py
+++ diario/views.py
@@ -1,19 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import Pessoa, Diario
 
 def home(request):
-    return render(request, 'home.html'),
+    textos = Diario.objects.order_by('create_at')[:3]
+    return render(request, 'home.html', {'textos': textos})
+
     
 def escrever(request):
     if request.method == "GET":
-        return render(request, 'escrever.html')  
-    elif request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        tags = request.POST.getlist('tags')
-        pessoas = request.POST.getlist('pessoas')
-        texto = request.POST.get('texto')
-        return HttpResponse(f'{titulo} - {tags} - {pessoas} -{texto}')
+        pessoas = Pessoa.objects.all()
+        textos = Diario.objects.order_by('create_at')   
+        return render(request, 'escrever.html', {'pessoas': pessoas, 'textos': textos})
+    else:
+        titulo = request.POST.get("titulo")
+        tags = request.POST.getlist("tags")
+        pessoas = request.POST.getlist("pessoas")
+        texto = request.POST.get("texto")
 
+        if len(titulo.strip()) == 0 or len(texto.strip()) == 0:
+            return redirect('escrever')
+
+        diario = Diario(
+            titulo=titulo,
+            texto=texto
+        )
+        diario.set_tags(tags)
+        diario.save()
         
-   
+        pessoa_objs = Pessoa.objects.filter(id__in=pessoas)
+        diario.pessoas.add(*pessoa_objs)
+        diario.save()
+
+        '''for i in pessoas:
+            pessoa = Pessoa.objects.get(id=i)
+            diario.pessoas.add(pessoa)'''
+
+        return redirect('escrever')
+       
+        
+def cadastrar_pessoa(request):
+    if request.method == 'GET':
+        return render(request, 'pessoa.html')  
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+    
+        pessoa = Pessoa(
+            nome=nome,
+            foto=foto
+        )
+        pessoa.save()
+        return redirect('escrever')
